37ab3c96c14d92f53ef2c53e20442fc7ac3b083b
tienluannguyen <nguyentienluan3@gmail.com>
2025-02-26 10:38:35 +0700

    change the currency of the widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,7 +12,15 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrencyData('BTC', selectedCurrency);
+  }
+
   String selectedCurrency = 'USD';
+  final String keyApi = 'bbcb56d1-4994-4c2a-b24e-1e9d99d0036f';
 
   Widget getAndroidButton() {
     List<DropdownMenuItem<String>> DropDownItem = [];
@@ -41,16 +52,41 @@ class _PriceScreenState extends State<PriceScreen> {
       itemExtent: 32,
       onSelectedItemChanged: (selectedIndex) {
         print(selectedIndex);
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
       },
       children: ScrollItem,
     );
   }
 
-  Widget AndroidOrIos () {
+  Widget AndroidOrIos() {
     if (Platform.isIOS) {
       return getIosButton();
     } else {
       return getAndroidButton();
+    }
+  }
+
+  Future<double> getCurrencyData(
+      String firstCurrency, String secondCurrency) async {
+    var url = await Uri.parse(
+        'https://api-realtime.exrates.coinapi.io/v1/exchangerate/$firstCurrency/$secondCurrency');
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'X-CoinAPI-Key': keyApi,
+        },
+      );
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return data['rate'] as double;
+      } else {
+        throw Exception('Failed to load exchange rate: ${response.statusCode} - ${response.body}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching exchange rate: $e');
     }
   }
 
@@ -76,7 +112,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ? $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
